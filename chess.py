0be88683f07e36f9709c316cbe9b65556b5ffb95
David Shao <dshao@Davids-MacBook-Pro-2.local>
2020-12-02 23:35:59 -0800

    Implement check and checkmate logic, use images for pieces

--- chess.py
+++ chess.py
@@ -4,9 +4,8 @@
 from cmu_112_graphics import *
 from abc import ABC, abstractmethod
 import time
-
-
-# Some of this code is not working yet...just a general template
+import copy
+import sys
 
 class Piece(ABC):
     def __init__(self,row,col,name,color):
@@ -20,18 +19,46 @@ class Piece(ABC):
     def isLegalMove(self,app,newRow,newCol):
         return False
     
-    def movePiece(self, app, newRow, newCol):
+    def movePiece(self, app, newRow, newCol, simulate = False):
+        tempRow = self.row
+        tempCol = self.col
         if newRow == self.row and newCol == self.col:
             return False
         if 0 <= newRow < 8 and 0 <= newCol <= 8 and self.isLegalMove(app, newRow, newCol):
             targetPiece = getPieceAtPosition(app, newRow, newCol)
-            if targetPiece:
+            if targetPiece and not simulate:
                 if targetPiece.color == self.color:
                     return False 
                 else:
                     app.pieces.remove(targetPiece)
             self.row = newRow
             self.col = newCol
+            if inCheck(app,self.color):
+                if targetPiece and targetPiece not in app.pieces:
+                    app.pieces.append(targetPiece)
+                self.row = tempRow
+                self.col = tempCol
+                
+                return False
+            if self.color == 'Black':
+                otherColor = 'White'
+            else:
+                otherColor = 'Black'
+            if inCheck(app, otherColor):
+                if not simulate:
+                    if checkMate(app, otherColor):
+                        app.gameOver = True
+                        if self.color == 'Black': 
+                            app.winner = 'White' 
+                            app.loser = 'Black'
+                        else:
+                            app.winner = 'Black'
+                            app.loser = 'White'
+                        print(f'{app.winner} checkmates {app.loser} ')
+
+                
+
+
             print(f'New Position: {newRow}, {newCol}')
             return True
         return False
@@ -116,6 +143,8 @@ class Rook(Piece):
 
 class King(Piece):
     def isLegalMove(self,app,newRow,newCol):
+        if  abs(self.row - newRow) <= 1 and abs(self.col - newCol) <= 1:
+            return True
         return False
 
 class Queen(Piece):
@@ -159,8 +188,6 @@ class Queen(Piece):
 
         return False     
         
-
-
 def appStarted(app):
     app.whiteTurn = True
     app.moves = []
@@ -180,6 +207,29 @@ def appStarted(app):
     initializeBoard(app)
     initializePieces(app)
     
+    # img src: 
+    # I scaled the images beforehand to 80 x 80, so I don't have to do it in the code
+    # I used https://onlinepngtools.com/create-transparent-png to make the images transparent
+
+    app.images = {}
+    app.images['BlackQueen'] = app.loadImage('./images/pieces/bQ.png')
+    app.images['BlackPawn'] = app.loadImage('./images/pieces/bP.png')
+    app.images['BlackBishop'] = app.loadImage('./images/pieces/bB.png')
+    app.images['BlackKnight'] = app.loadImage('./images/pieces/bN.png')
+    app.images['BlackRook'] = app.loadImage('./images/pieces/bR.png')
+    app.images['BlackKing'] = app.loadImage('./images/pieces/bK.png')
+
+    app.images['WhiteQueen'] = app.loadImage('./images/pieces/wQ.png')
+    app.images['WhitePawn'] = app.loadImage('./images/pieces/wP.png')
+    app.images['WhiteBishop'] = app.loadImage('./images/pieces/wB.png')
+    app.images['WhiteKnight'] = app.loadImage('./images/pieces/wN.png')
+    app.images['WhiteRook'] = app.loadImage('./images/pieces/wR.png')
+    app.images['WhiteKing'] = app.loadImage('./images/pieces/wK.png')
+
+
+
+
+    
 def initializeBoard(app):
     # initializes the board without pieces and with alternating color scheme
     for rows in range(app.rows):
@@ -193,6 +243,17 @@ def initializeBoard(app):
             row.append(app.startGrey)
             app.startGrey = not app.startGrey
         app.board.append(row)
+
+def drawCoords(app,canvas):
+    if app.whiteTurn:
+        for row in range(len(app.board)):
+            canvas.create_text(8,row * app.cellSize + 15, text = f'{8 - row}', fill = '#99FFCC')
+            canvas.create_text(app.cellSize - 16 + row * app.cellSize,app.height - 20, text = chr(ord('a') + row), fill = '#99FFCC')
+    
+    else:
+        for row in range(len(app.board)):
+            canvas.create_text(app.height - 8,row * app.cellSize + 15, text = f'{row + 1}', fill = '#99FFCC')
+            canvas.create_text(16 + row * app.cellSize,app.height - 20, text = chr(ord('h') - row), fill = '#99FFCC')
 
 def initializePieces(app):
     # Rook
@@ -224,22 +285,49 @@ def initializePieces(app):
     
     # All the pawns
     for piece in range(len(app.pawns)):
-        # app.pieces.append((app.secondLevel[piece],1,piece,'black'))
-        # app.WhitePieces.append((app.secondLevel[piece],len(app.board) - 1 - 1,piece,'white'))
         app.pieces.append(Pawn(1,piece,"Pawn","Black"))
         app.pieces.append(Pawn(len(app.board) - 1 - 1,piece,"Pawn","White"))
+
+def inCheck(app,color):
+    kingRow, kingCol = kingPosition(app,color)
+    for piece in app.pieces:
+        if piece.color != color and piece.isLegalMove(app,kingRow,kingCol):
+            return True
+    return False
+
+
+def checkMate(app, color):
+    ''' Check if player with given color is in checkmate by the other player
+    returns True if it is in checkMate, false otherwise
+    precondition: the player is in check
+    '''
+    for piece in app.pieces:
+        tempRow = piece.row
+        tempCol = piece.col
+        if piece.color == color:
+            for row in range(len(app.board)):
+                for col in range(len(app.board[0])):
+                    # Pass in True to let the method know that we are merely simulating a move, not actually making one to avoid stack overflow
+                    if piece.movePiece(app,row,col,True):
+                        piece.row = tempRow
+                        piece.col = tempCol
+                        return False
+    return True
 
 def drawPieces(app,canvas):
     for piece in app.pieces:
         x0,y0,x1,y1 = getCellBounds(app,piece.row,piece.col)
         midx = (x0 + x1) // 2
         midy = (y0 + y1) // 2
-        canvas.create_text(midx, midy, text = piece.name, fill = piece.color)
+        imageKey = piece.color + piece.name
+        # Image Method from https://www.cs.cmu.edu/~112/notes/notes-animations-part3.html 
+        canvas.create_image(midx, midy, image=ImageTk.PhotoImage(app.images[imageKey]))
     
     
-# every time player makes a move, the board and pieces must flip
+# every time player makes a move, the pieces must flip to keep the perspective right
 
 def flipBoard(app):
+    '''
     tempBoard = []
     for row in range(len(app.board)):
         tempRow = []
@@ -251,16 +339,27 @@ def flipBoard(app):
         for col in range(len(app.board[0])):
             tempBoard[row][col] = app.board[len(app.board) - 1 - row][col]
     app.board = tempBoard
+    '''
     app.outlineRow = len(app.board) - 1 - app.outlineRow
+    app.outlineCol = len(app.board[0]) - 1 - app.outlineCol
+
+# find out where the king is
+def kingPosition(app,color):
+    for piece in app.pieces:
+        if piece.color == color and piece.name == 'King':
+                kingRow = piece.row
+                kingCol = piece.col
+                return (kingRow, kingCol)
     
 def flipPieces(app):
     for piece in app.pieces:
        piece.row = len(app.board) - piece.row - 1
+       piece.col = len(app.board[0]) - piece.col - 1
 
 def keyPressed(app, event):  
-    if (event.key == 'f'):
-        flipBoard(app)
-        flipPieces(app)
+    if (event.key == 'r') and app.gameOver:
+        appStarted(app)
+       
     if (event.key== 's'):
         from tkinter.filedialog import asksaveasfile 
         files = [('All Files', '*.*'),  
@@ -284,6 +383,7 @@ def mousePressed(app, event):
                 flipBoard(app)
                 flipPieces(app)
                 app.whiteTurn = not app.whiteTurn
+                app.moves.append((row,col,app.selectedPiece.color))
                 return
                         
     app.selectedPiece = getPieceAtPosition(app, row, col)
@@ -294,6 +394,18 @@ def mousePressed(app, event):
 def getCell(app, x, y):
     return int(y/app.cellSize), int(x/app.cellSize)
 
+def drawMoves(app,canvas):
+    for i in range(len(app.moves)):
+        if app.moves[i][2] == 'White':
+            player = 'White'
+            row = 8 - app.moves[i][0] 
+            col = chr(ord('a') + app.moves[i][1])
+        else:
+            player = 'Black'
+            col = chr(ord('h') - app.moves[i][1])
+            row = app.moves[i][0] + 1
+        canvas.create_text(app.height + (app.width - app.height) // 2, 20 + app.cellSize * i, text = f'{player} to {col}{row}')
+      
 def drawBoard(app,canvas):
     for row in range(len(app.board)):
         for col in range(len(app.board[0])):
@@ -305,12 +417,14 @@ def drawBoard(app,canvas):
             if (app.outlineRow or app.outlineCol != None) and (row,col) == (app.outlineRow, app.outlineCol):
                 canvas.create_rectangle(x0,y0,x1-1,y1-1,fill = color, outline = 'yellow')
             else:
-                canvas.create_rectangle(x0,y0,x1,y1,fill = color, outline = '') 
+                canvas.create_rectangle(x0,y0,x1,y1,fill = color, outline = 'black') 
                
 
 def redrawAll(app,canvas):
     drawBoard(app,canvas)
     drawPieces(app,canvas)
+    drawCoords(app,canvas)
+    drawMoves(app,canvas)
 
 def getCellBounds(app,row,col):
     x0 = col * app.cellSize
